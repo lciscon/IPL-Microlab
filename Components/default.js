5b81c4c2db7c1820d2a07dd564c32e6e37c3be2d
lciscon <lciscon@yahoo.com>
2023-01-14 18:32:04 -0600

    Update default.js

--- Components/default.js
+++ Components/default.js
@@ -4,13 +4,13 @@ import { Octokit, App } from "https://cdn.skypack.dev/octokit";
 const octokit2 = new Octokit()
 
 async function update_files() {
-  var files = octokit2.rest.repos.getContent({
+  var file = octokit2.rest.repos.getContent({
     owner: 'lciscon',
     repo: 'IPL-Microlab',
     path: 'Components'
   });
   
-  console.log('Files found at root level', files.data.map((file) => file.name));  
+  console.log('Files found at root level', file.data.map((file) => file.name));  
 }
 
 
