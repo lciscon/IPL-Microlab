43ec51630a01d67e4c082fe34ddd8a4fbc1f1ae4
lciscon <lciscon@yahoo.com>
2023-01-14 21:13:31 -0600

    Update default.js

--- Components/default.js
+++ Components/default.js
@@ -12,7 +12,7 @@ async function update_files() {
   
   var files2 = files.data.filter((function (el) {
                   return el.type == 'file'
-                  }).map((file) => file.name))
+                  })).map((file) => file.name))
          
  console.log('Files found at root level', files2);  
 //  console.log('Files found at root level', files.data.map((file) => file.name));  
